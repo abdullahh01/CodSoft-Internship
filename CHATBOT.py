026bd71f55834ec27045e399876952937a21de54
Abdullah Hashim <abdullahhash01@gmail.com>
2023-09-10 21:18:29 +0500

    Task 1

--- CHATBOT.py
+++ CHATBOT.py
@@ -1,18 +1,21 @@
 #CHATBOTTASK1
+
 import re
+
 def simple_chatbot(user_input):
-    if re.search(r'\bHello\b', user_input, re.I):
+    if re.search(r'\bhello\b', user_input, re.I):
         return "Hello! How can I help you?"
-    elif re.search(r'\bWho are you\b', user_input, re.I):
+    elif re.search(r'\bwho are you\b', user_input, re.I):
         return "I'm  a chatbot, and  I'm here to assist you!"
     elif re.search(r'\bWho is your boss?\b', user_input, re.I):
         return "Abdullah Hashim, is my boss !"
     elif re.search(r'\bWhat can you do here?\b', user_input, re.I):
          return "I'm  a chatbot, and  I'm here to assist you!"
-    elif re.search(r'\bBye\b', user_input, re.I):
+    elif re.search(r'\bbye\b', user_input, re.I):
         return "Goodbye! Have a great day!"
     else:
         return "I'm sorry, I don't understand that."
+
 
 while True:
     user_input = input("You: ")
